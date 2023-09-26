d60bf5bff971042a57e2f816868c62112dc56886
hsh_519 <suk000519@naver.com>
2023-09-26 14:57:56 +0900

    toychat 프로젝트 생성

---
+++ src/main/java/com/example/toychat/ToychatApplication.java
@@ -0,0 +1,13 @@
+package com.example.toychat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToychatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToychatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/toychat/ToychatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.toychat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToychatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
